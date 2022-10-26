8f2be501b879c1709ab233ec00c949bde7a27113
Jyanx <unlimitedzx9@gmail.com>
2022-10-26 17:21:35 -0500

    Tarea

--- Semana5/Dia 2/funciones.js
+++ Semana5/Dia 2/funciones.js
@@ -16,3 +16,48 @@ function sumar (numero1, numero2){
     const suma = convertNumero1 + convertNumero2;
     console.log("Es el resultado: ", suma);
 }
+
+
+// tipos de datos
+
+let nombre = "Eduardo" //string
+let age = 34 // number
+let talla = 1.75 // float
+let boll = true // boolean
+let indefinido; // no definido
+let nulo = null;
+let simbolo = symbol("zcd");
+let object = {};
+
+console.log(nombre, typeof(nombre))
+console.log(age,typeof(age))
+console.log(boll,typeof(boll))
+
+// cambiar el tipo de dato
+
+console.log(age,typeof toString(age))
+console.log(nombre,typeof parseInt(nombre))
+console.log(talla,typeof parseFloat(talla))
+
+//tipos de funciones 
+//funcion expresiva 
+function suma3 (a,b){
+    return a+b
+}
+
+//funcion anonima 
+
+let restar = function (x,y){
+    return x-y
+}
+
+// funcion flecha
+
+let multiplicar =(x,y)=>{
+    return x*y
+}
+
+
+console.log("La suma es: ",suma3);
+console.log("La resta es: ",suma3);
+console.log("La multiplicacion es: ",suma3);
